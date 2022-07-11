c6db2c80d481d45b722e842a368ca9149aa3555c
Krystof Janicek <janicek.kryst@gmail.com>
2022-07-11 14:14:08 +0200

    217 ContainsDuplicate

---
+++ Solutions/ArraysAndHashing/LC217_ContainsDuplicate.cs
@@ -0,0 +1,46 @@
+﻿//https://leetcode.com/problems/contains-duplicate/
+
+namespace NeetCode.Solutions.ArraysAndHashing;
+
+public class LC217_ContainsDuplicate
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var processedNumbers = new HashSet<int>();
+        foreach (var number in nums)
+        {
+            if (processedNumbers.Contains(number))
+                return true;
+            processedNumbers.Add(number);
+        }
+        return false;
+    }
+
+    public bool FancyContainsDuplicate(int[] nums)
+    {
+        var numbers = new HashSet<int>(nums);
+        return numbers.Count != nums.Length;
+    }
+
+    public bool FasterContainsDuplicate(int[] nums)
+    {
+        var processedNumbers = new HashSet<int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var num = nums[i];
+            if (processedNumbers.Contains(num))
+                return true;
+            processedNumbers.Add(num);
+        }
+        return false;
+    }
+
+    public void DoMeasurement()
+    {
+        var nums = new[] { 1, 2, 3, 4 };
+        var measurement = new Measurement();
+        measurement.Measure("ContainsDuplicate", () => { ContainsDuplicate(nums); });
+        measurement.Measure("FasterContainsDuplicate", () => { FasterContainsDuplicate(nums); });
+        measurement.Measure("FancyContainsDuplicate", () => { FancyContainsDuplicate(nums); });
+    }
+}
---
+++ Tests/ArraysAndHashing/LC217_ContainsDuplicateTests.cs
@@ -0,0 +1,84 @@
+﻿using NeetCode.Solutions.ArraysAndHashing;
+
+namespace NeetCode.Tests.ArraysAndHashing;
+
+[TestClass]
+public class LC217_ContainsDuplicateTests
+{
+    [TestMethod]
+    public void HasDuplicity_ReturnsTrue()
+    {
+        var numbers = new int[] { 1, 2, 3, 1 };
+
+        var result = ContainsDuplicate(numbers);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void NoDuplicity_ReturnsFalse()
+    {
+        var numbers = new int[] { 1, 2, 3, 4 };
+
+        var result = ContainsDuplicate(numbers);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void HasMultipleDuplicities_ReturnsTrue()
+    {
+        var numbers = new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+
+        var result = ContainsDuplicate(numbers);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void NoDuplicity_OnlyOneNumber_ReturnsFalse()
+    {
+        var numbers = new int[] { 5 };
+
+        var result = ContainsDuplicate(numbers);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void NoDuplicity_NegativeNumbers_ReturnsFalse()
+    {
+        var numbers = new int[] { -1, 1, 2, -9, 3, -7, 4, 0 };
+
+        var result = ContainsDuplicate(numbers);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void HasDuplicity_NegativeNumbers_ReturnsTrue()
+    {
+        var numbers = new int[] { -1, -2, -3, -4, -1 };
+
+        var result = ContainsDuplicate(numbers);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void HasDuplicity_PositiveAndNegativeZero_ReturnsTrue()
+    {
+        var numbers = new int[] { 0, -0 };
+
+        var result = ContainsDuplicate(numbers);
+
+        Assert.IsTrue(result);
+    }
+
+    private bool ContainsDuplicate(int[] numbers)
+    {
+        var x = new LC217_ContainsDuplicate();
+        var result = x.ContainsDuplicate(numbers);
+        return result;
+    }
+}
