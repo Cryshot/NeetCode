1770e2518d9d203cf0b730dfbbaa592900b5b8ed
Krystof Janicek <janicek.kryst@gmail.com>
2022-07-12 17:52:57 +0200

    347 Top K Frequent Elements

---
+++ Solutions/ArraysAndHashing/LC347_TopKFrequentElements.cs
@@ -0,0 +1,47 @@
+﻿namespace NeetCode.Solutions.ArraysAndHashing;
+
+public class LC347_TopKFrequentElements
+{
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        return TopKFrequent_II(nums, k);
+    }
+
+    private static int[] TopKFrequent_I(int[] nums, int k)
+    {
+        var frequency = new Dictionary<int, int>();
+        foreach (var number in nums)
+        {
+            if (!frequency.ContainsKey(number))
+                frequency.Add(number, 0);
+            frequency[number]++;
+        }
+
+        var result = frequency.OrderByDescending(x => x.Value).Select(x => x.Key).Take(k).ToArray();
+        return result;
+    }
+
+    private static int[] TopKFrequent_II(int[] nums, int k)
+    {
+        var frequency = new Dictionary<int, int>();
+        foreach (var number in nums)
+        {
+            if (!frequency.ContainsKey(number))
+                frequency.Add(number, 0);
+            frequency[number]++;
+        }
+
+        var queue = new PriorityQueue<int, int>();
+        foreach (var pair in frequency)
+        {
+            queue.Enqueue(pair.Key, pair.Value);
+            if (queue.Count > k)
+                queue.Dequeue();
+        }
+
+        var result = new int[k];
+        for (var i = 0; queue.Count > 0; i++)
+            result[i] = queue.Dequeue();
+        return result;
+    }
+}
---
+++ Tests/ArraysAndHashing/LC347_TopKFrequentElementsTests.cs
@@ -0,0 +1,69 @@
+﻿using NeetCode.Solutions.ArraysAndHashing;
+
+namespace NeetCode.Tests.ArraysAndHashing;
+
+[TestClass]
+public class LC347_TopKFrequentElementsTests
+{
+    [TestMethod]
+    public void MultipleNumbers_TwoTopFrequent()
+    {
+        var nums = new int[] { 1, 1, 1, 2, 2, 3 };
+        var k = 2;
+
+        var result = TopKFrequent(nums, k);
+
+        CollectionAssert.AreEquivalent(new int[] { 1, 2 }, result);
+    }
+
+    [TestMethod]
+    public void OneNumber_OneTopFrequent()
+    {
+        var nums = new int[] { 1 };
+        var k = 1;
+
+        var result = TopKFrequent(nums, k);
+
+        CollectionAssert.AreEquivalent(new int[] { 1 }, result);
+    }
+
+    [TestMethod]
+    public void TwoNumbers_SameFrequency_TwoTopFrequent()
+    {
+        var nums = new int[] { 1, 1, 1, 2, 2, 2 };
+        var k = 2;
+
+        var result = TopKFrequent(nums, k);
+
+        CollectionAssert.AreEquivalent(new int[] { 1, 2 }, result);
+    }
+
+    [TestMethod]
+    public void MultipleNumbers_OneTopFrequent()
+    {
+        var nums = new int[] { 0, 4, 1, 4, 2, 4 };
+        var k = 1;
+
+        var result = TopKFrequent(nums, k);
+
+        CollectionAssert.AreEquivalent(new int[] { 4 }, result);
+    }
+
+    [TestMethod]
+    public void MultipleNumbers_FourTopFrequent()
+    {
+        var nums = new int[] { 1, 2, 3, 3, 3, 4, 5, 6, 1, 1, 2, 3, 4, 9, 7, 4, 8, 4, 4 };
+        var k = 4;
+
+        var result = TopKFrequent(nums, k);
+
+        CollectionAssert.AreEquivalent(new int[] { 1, 2, 3, 4 }, result);
+    }
+
+    private int[] TopKFrequent(int[] nums, int k)
+    {
+        var @object = new LC347_TopKFrequentElements();
+        var result = @object.TopKFrequent(nums, k);
+        return result;
+    }
+}
