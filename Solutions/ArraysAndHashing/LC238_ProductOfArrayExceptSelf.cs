1a9536b763571055fd662211bc5ea6f7793e1012
Krystof Janicek <janicek.kryst@gmail.com>
2022-07-13 11:28:11 +0200

    238 Product of Array Except Self

---
+++ Solutions/ArraysAndHashing/LC238_ProductOfArrayExceptSelf.cs
@@ -0,0 +1,82 @@
+﻿namespace NeetCode.Solutions.ArraysAndHashing;
+
+public class LC238_ProductOfArrayExceptSelf
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        return ProductExceptSelf_III(nums);
+    }
+
+    private static int[] ProductExceptSelf_I(int[] nums)
+    {
+        var result = Enumerable.Repeat(1, nums.Length).ToArray();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var j = 0; j < nums.Length; j++)
+            {
+                if (i != j)
+                    result[j] *= nums[i];
+            }
+        }
+
+        return result;
+    }
+
+    private static int[] ProductExceptSelf_II(int[] nums)
+    {
+        var numsFrequency = new Dictionary<int, int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (!numsFrequency.ContainsKey(nums[i]))
+                numsFrequency.Add(nums[i], 0);
+            numsFrequency[nums[i]]++;
+        }
+
+        var result = new int[nums.Length];
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var product = 1;
+            foreach (var pair in numsFrequency)
+            {
+                if (pair.Key == 0 && (nums[i] != 0 || pair.Value > 1))
+                {
+                    product = 0;
+                    break;
+                }
+
+                var numberFrequency = pair.Value;
+                if (nums[i] == pair.Key)
+                    numberFrequency--;
+
+                if (numberFrequency > 0)
+                    product *= (int)Math.Pow(pair.Key, numberFrequency);
+            }
+
+            result[i] = product;
+        }
+
+        return result;
+    }
+
+    private static int[] ProductExceptSelf_III(int[] nums)
+    {
+        var result = new int[nums.Length];
+
+        var prefix = 1;
+        var suffix = 1;
+        for (int i = 0, j = nums.Length - 1; i < nums.Length; i++, j--)
+        {
+            if (i <= j)
+                result[i] = result[j] = 1;
+
+            result[i] *= prefix;
+            result[j] *= suffix;
+
+            prefix *= nums[i];
+            suffix *= nums[j];
+        }
+
+        return result;
+    }
+}
---
+++ Tests/ArraysAndHashing/LC238_ProductOfArrayExceptSelfTests.cs
@@ -0,0 +1,60 @@
+﻿using NeetCode.Solutions.ArraysAndHashing;
+
+namespace NeetCode.Tests.ArraysAndHashing;
+
+[TestClass]
+public class LC238_ProductOfArrayExceptSelfTests
+{
+    [TestMethod]
+    public void BasicMultiplication()
+    {
+        var nums = new int[] { 1, 2, 3, 4 };
+        var expected = new int[] { 24, 12, 8, 6 };
+        DoTest(nums, expected);
+    }
+
+    [TestMethod]
+    public void MultiplicationWithZero_OnlyOneProductNotZero()
+    {
+        var nums = new int[] { -1, 1, 0, -3, 3 };
+        var expected = new int[] { 0, 0, 9, 0, 0 };
+        DoTest(nums, expected);
+    }
+
+    [TestMethod]
+    public void MinAmountOfNumbers_Zeroes()
+    {
+        var nums = new int[] { 0, 0 };
+        var expected = new int[] { 0, 0 };
+        DoTest(nums, expected);
+    }
+
+    [TestMethod]
+    public void MultiplicationWithOnes()
+    {
+        var nums = new int[] { 1, 1, 1 };
+        var expected = new int[] { 1, 1, 1 };
+        DoTest(nums, expected);
+    }
+
+    [TestMethod]
+    public void NegativeMultiplication()
+    {
+        var nums = new int[] { -1, -2, -3, 4 };
+        var expected = new int[] { 24, 12, 8, -6 };
+        DoTest(nums, expected);
+    }
+
+    private void DoTest(int[] nums, int[] expected)
+    {
+        var result = ProductExceptSelf(nums);
+        CollectionAssert.AreEqual(expected, result);
+    }
+
+    private int[] ProductExceptSelf(int[] nums)
+    {
+        var @object = new LC238_ProductOfArrayExceptSelf();
+        var result = @object.ProductExceptSelf(nums);
+        return result;
+    }
+}
