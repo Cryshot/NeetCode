22bbb2508603d0245cbbc5583519bde36d1352c7
Krystof Janicek <janicek.kryst@gmail.com>
2022-07-15 16:06:38 +0200

    167 Two Sum II - Input Array Is Sorted

---
+++ Solutions/TwoPointers/LC167_TwoSumII-InputArrayIsSorted.cs
@@ -0,0 +1,58 @@
+﻿namespace NeetCode.Solutions.TwoPointers;
+
+public class LC167_TwoSumII_InputArrayIsSorted
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        return TwoSum_II(numbers, target);
+    }
+
+    private static int[] TwoSum_I(int[] numbers, int target)
+    {
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            var result = target - numbers[i];
+            var resultIndex = IntervalHalvingSearch(numbers, i + 1, numbers.Length - 1, result);
+
+            if (resultIndex != -1)
+                return new int[] { i + 1, resultIndex + 1 };
+        }
+
+        throw new Exception($"Solution doesn't exist");
+    }
+
+    private static int IntervalHalvingSearch(int[] numbers, int start, int end, int result)
+    {
+        var half = start + (end - start) / 2;
+        if (numbers[half] == result)
+            return half;
+
+        if (start == end || start + 1 == end)
+            return (numbers[end] == result) ? end : -1;
+
+        if (numbers[half] > result)
+            return IntervalHalvingSearch(numbers, start, half, result);
+        else
+            return IntervalHalvingSearch(numbers, half, end, result);
+    }
+
+    private static int[] TwoSum_II(int[] numbers, int target)
+    {
+        var left = 0;
+        var right = numbers.Length - 1;
+
+        while (left < right)
+        {
+            var sum = numbers[left] + numbers[right];
+            if (sum == target)
+                return new int[] { left + 1, right + 1 };
+
+            if (sum < target)
+                left++;
+            else if (sum > target)
+                right--;
+        }
+
+        throw new Exception($"Solution doesn't exist");
+    }
+}
---
+++ Tests/TwoPointers/LC167_TwoSumII-InputArrayIsSortedTests.cs
@@ -0,0 +1,113 @@
+﻿using NeetCode.Solutions.TwoPointers;
+
+namespace NeetCode.Tests.TwoPointers;
+
+[TestClass]
+public class LC167_TwoSumII_InputArrayIsSortedTests
+{
+    [TestMethod]
+    public void FourPositiveNumbers()
+    {
+        var numbers = new int[] { 2, 7, 11, 15 };
+        var target = 9;
+
+        var result = TwoSum(numbers, target);
+
+        CollectionAssert.AreEquivalent(new int[] { 1, 2 }, result);
+    }
+
+    [TestMethod]
+    public void ThreePositiveNumbers()
+    {
+        var numbers = new int[] { 2, 3, 4 };
+        var target = 6;
+
+        var result = TwoSum(numbers, target);
+
+        CollectionAssert.AreEquivalent(new int[] { 1, 3 }, result);
+    }
+
+    [TestMethod]
+    public void ZeroAndNegativeNumber()
+    {
+        var numbers = new int[] { -1, 0 };
+        var target = -1;
+
+        var result = TwoSum(numbers, target);
+
+        CollectionAssert.AreEquivalent(new int[] { 1, 2 }, result);
+    }
+
+    [TestMethod]
+    public void NegativeNumbers()
+    {
+        var numbers = new int[] { -4, -3, -2, -1 };
+        var target = -3;
+
+        var result = TwoSum(numbers, target);
+
+        CollectionAssert.AreEquivalent(new int[] { 3, 4 }, result);
+    }
+
+    [TestMethod]
+    public void NegativeAndPositiveNumbers()
+    {
+        var numbers = new int[] { -3, -2, 0, 2 };
+        var target = 0;
+
+        var result = TwoSum(numbers, target);
+
+        CollectionAssert.AreEquivalent(new int[] { 2, 4 }, result);
+    }
+
+    [TestMethod]
+    public void BigNumberTarget()
+    {
+        var numbers = new int[] { 3, 24, 50, 79, 88, 150, 345 };
+        var target = 200;
+
+        var result = TwoSum(numbers, target);
+
+        CollectionAssert.AreEquivalent(new int[] { 3, 6 }, result);
+    }
+
+    [TestMethod]
+    public void InputContainsSameNumbers()
+    {
+        var numbers = new int[] { 1, 3, 4, 4 };
+        var target = 8;
+
+        var result = TwoSum(numbers, target);
+
+        CollectionAssert.AreEquivalent(new int[] { 3, 4 }, result);
+    }
+
+    [TestMethod]
+    [Ignore("Task constraint eliminates overflow")]
+    [ExpectedException(typeof(OverflowException))]
+    public void NegativeOverflow_ThrowsOverflowException()
+    {
+        var numbers = new int[] { int.MinValue, -2, 0, 1 };
+        var target = int.MinValue;
+
+        var result = TwoSum(numbers, target);
+    }
+
+    [TestMethod]
+    [Ignore("Task constraint eliminates overflow")]
+    [ExpectedException(typeof(OverflowException))]
+    public void PositiveOverflow_NoSolution_ThrowsOverflowException()
+    {
+        var numbers = new int[] { 1, 2, int.MaxValue };
+        var target = int.MaxValue;
+
+        var result = TwoSum(numbers, target);
+    }
+
+    private static int[] TwoSum(int[] numbers, int target)
+    {
+        var @object = new LC167_TwoSumII_InputArrayIsSorted();
+        var result = @object.TwoSum(numbers, target);
+        return result;
+    }
+}
