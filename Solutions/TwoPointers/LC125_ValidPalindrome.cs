7349ee8afd55d9b43f209fbb08cbfe410cdaa6fc
Krystof Janicek <janicek.kryst@gmail.com>
2022-07-15 14:19:13 +0200

    125 Valid Palindrome

---
+++ Solutions/TwoPointers/LC125_ValidPalindrome.cs
@@ -0,0 +1,31 @@
+﻿namespace NeetCode.Solutions.TwoPointers;
+
+public class LC125_ValidPalindrome
+{
+    public bool IsPalindrome(string s)
+    {
+        return IsPalindrome_I(s);
+    }
+
+    private static bool IsPalindrome_I(string s)
+    {
+        var letters = new List<char>(s.Length);
+        for (var i = 0; i < s.Length; i++)
+        {
+            var @char = char.ToLower(s[i]);
+            if ((@char >= 'a' && @char <= 'z') || (@char >= '0' && @char <= '9'))
+                letters.Add(@char);
+        }
+
+        if (letters.Count == 0)
+            return true;
+
+        for (var i = 0; i < letters.Count; i++)
+        {
+            if (letters[i] != letters[letters.Count - 1 - i])
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ Tests/TwoPointers/LC125_ValidPalindromeTests.cs
@@ -0,0 +1,104 @@
+﻿using NeetCode.Solutions.TwoPointers;
+
+namespace NeetCode.Tests.TwoPointers;
+
+[TestClass]
+public class LC125_ValidPalindromeTests
+{
+    [TestMethod]
+    public void ValidPalindrome_ReturnsTrue()
+    {
+        var s = "A man, a plan, a canal: Panama";
+
+        var result = IsPalindrome(s);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void InvalidPalindrome_ReturnsFalse()
+    {
+        var s = "race a car";
+
+        var result = IsPalindrome(s);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void WhiteSpaceInput_ReturnsTrue()
+    {
+        var s = " ";
+
+        var result = IsPalindrome(s);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void NonAlphaNumericInput_ReturnsTrue()
+    {
+        var s = "--....-¨§§)))===.¨´´´¨¨";
+
+        var result = IsPalindrome(s);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void MultipleWhiteSpaceInput_ReturnsTrue()
+    {
+        var s = "                 ";
+
+        var result = IsPalindrome(s);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void ComplexValidPalindrome_ReturnsTrue()
+    {
+        var s = "Are we not pure? “No, sir!” Panama’s moody Noriega brags. “It is garbage!” Irony dooms a man—a prisoner up to new era.";
+
+        var result = IsPalindrome(s);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void LongValidPalindrome_ReturnsTrue()
+    {
+        var s = "Dennis, Nell, Edna, Leon, Nedra, Anita, Rolf, Nora, Alice, Carol, Leo, Jane, Reed, Dena, Dale, Basil, Rae, Penny, Lana, Dave, Denny, Lena, Ida, Bernadette, Ben, Ray, Lila, Nina, Jo, Ira, Mara, Sara, Mario, Jan, Ina, Lily, Arne, Bette, Dan, Reba, Diane, Lynn, Ed, Eva, Dana, Lynne, Pearl, Isabel, Ada, Ned, Dee, Rena, Joel, Lora, Cecil, Aaron, Flora, Tina, Arden, Noel, and Ellen sinned.";
+
+        var result = IsPalindrome(s);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void AlphaNumericValidPalindrome_ReturnsTrue()
+    {
+        var s = "123aha321";
+
+        var result = IsPalindrome(s);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void AlphaNumericValidPalindrome_ReturnsFalse()
+    {
+        var s = "12aha321";
+
+        var result = IsPalindrome(s);
+
+        Assert.IsFalse(result);
+    }
+
+    private static bool IsPalindrome(string s)
+    {
+        var @object = new LC125_ValidPalindrome();
+        var result = @object.IsPalindrome(s);
+        return result;
+    }
+}
