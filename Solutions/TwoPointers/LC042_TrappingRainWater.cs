3c37edee666d1c1316311df1796ed1d9e0d67363
Krystof Janicek <janicek.kryst@gmail.com>
2022-07-22 17:53:33 +0200

    42 Trapping Rain Water

---
+++ Solutions/TwoPointers/LC042_TrappingRainWater.cs
@@ -0,0 +1,42 @@
+﻿namespace NeetCode.Solutions.TwoPointers;
+
+public class LC042_TrappingRainWater
+{
+    public int Trap(int[] height)
+    {
+        return Trap_I(height);
+    }
+
+    private static int Trap_I(int[] height)
+    {
+        var waterSum = 0;
+
+        var left = 0;
+        var right = height.Length - 1;
+
+        var leftMax = 0;
+        var rightMax = 0;
+
+        while (left < right)
+        {
+            if (height[left] < height[right])
+            {
+                if (leftMax < height[left])
+                    leftMax = height[left];
+                else
+                    waterSum += leftMax - height[left];
+                left++;
+            }
+            else
+            {
+                if (rightMax < height[right])
+                    rightMax = height[right];
+                else
+                    waterSum += rightMax - height[right];
+                right--;
+            }
+        }
+
+        return waterSum;
+    }
+}
---
+++ Tests/TwoPointers/LC042_TrappingRainWaterTests.cs
@@ -0,0 +1,54 @@
+﻿using NeetCode.Solutions.TwoPointers;
+
+namespace NeetCode.Tests.TwoPointers;
+
+[TestClass]
+public class LC042_TrappingRainWaterTests
+{
+    [TestMethod]
+    public void Example1()
+    {
+        var height = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+
+        var result = Trap(height);
+
+        Assert.AreEqual(6, result);
+    }
+
+    [TestMethod]
+    public void Example2()
+    {
+        var height = new int[] { 4, 2, 0, 3, 2, 5 };
+
+        var result = Trap(height);
+
+        Assert.AreEqual(9, result);
+    }
+
+    [TestMethod]
+    public void Example3()
+    {
+        var height = new int[] { 4, 2, 3, 2 };
+
+        var result = Trap(height);
+
+        Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void Example4()
+    {
+        var height = new int[] { 4, 2, 2, 3 };
+
+        var result = Trap(height);
+
+        Assert.AreEqual(2, result);
+    }
+
+    private static int Trap(int[] height)
+    {
+        var @object = new LC042_TrappingRainWater();
+        var result = @object.Trap(height);
+        return result;
+    }
+}
