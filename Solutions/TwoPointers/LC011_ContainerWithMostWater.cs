6d170721845ff3472f094f4842c433b360f39640
Krystof Janicek <janicek.kryst@gmail.com>
2022-07-22 16:28:15 +0200

    11 Container With Most Water

---
+++ Solutions/TwoPointers/LC011_ContainerWithMostWater.cs
@@ -0,0 +1,79 @@
+﻿namespace NeetCode.Solutions.TwoPointers;
+
+public class LC011_ContainerWithMostWater
+{
+    public int MaxArea(int[] height)
+    {
+        return MaxArea_III(height);
+    }
+
+    private static int MaxArea_I(int[] height)
+    {
+        var maxArea = int.MinValue;
+        for (var i = 0; i < height.Length - 1; i++)
+        {
+            for (var j = i + 1; j < height.Length; j++)
+            {
+                var area = (j - i) * Math.Min(height[i], height[j]);
+                if (area > maxArea)
+                    maxArea = area;
+            }
+        }
+
+        return maxArea;
+    }
+
+    private static int MaxArea_II(int[] height)
+    {
+        var dict = new SortedDictionary<int, int>();
+        for (var i = 0; i < height.Length; i++)
+        {
+            if (!dict.ContainsKey(height[i]))
+                dict.Add(height[i], 0);
+            dict[height[i]] = i;
+        }
+
+        var maxArea = 0;
+        for (var i = 0; i < height.Length; i++)
+        {
+            if (height[i] == 0)
+                continue;
+
+            for (var j = dict.Count - 1; j >= 0; j--)
+            {
+                var key = dict.Keys.ElementAt(j);
+                var minHeight = Math.Min(height[i], key);
+
+                if (minHeight * (height.Length - 1 - i) <= maxArea)
+                    break;
+
+                var area = minHeight * Math.Abs(i - dict[key]);
+                if (area > maxArea)
+                    maxArea = area;
+            }
+        }
+
+        return maxArea;
+    }
+
+    private static int MaxArea_III(int[] height)
+    {
+        var left = 0;
+        var right = height.Length - 1;
+        var maxArea = 0;
+
+        while (left < right)
+        {
+            var minHeight = Math.Min(height[left], height[right]);
+            var area = minHeight * (right - left);
+            maxArea = Math.Max(maxArea, area);
+
+            if (height[left] > height[right])
+                right--;
+            else
+                left++;
+        }
+
+        return maxArea;
+    }
+}
---
+++ Tests/TwoPointers/LC011_ContainerWithMostWaterTests.cs
@@ -0,0 +1,84 @@
+﻿using NeetCode.Solutions.TwoPointers;
+
+namespace NeetCode.Tests.TwoPointers;
+
+[TestClass]
+public class LC011_ContainerWithMostWaterTests
+{
+    [TestMethod]
+    public void MultipleDifferentHeight_ReturnsFortyNine()
+    {
+        var height = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+
+        var result = MaxArea(height);
+
+        Assert.AreEqual(49, result);
+    }
+
+    [TestMethod]
+    public void HeightOne_ReturnsOne()
+    {
+        var height = new int[] { 1, 1 };
+
+        var result = MaxArea(height);
+
+        Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void HeightTwoOne_ReturnsOne()
+    {
+        var height = new int[] { 2, 1 };
+
+        var result = MaxArea(height);
+
+        Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void HeightTwoZeroTwo_ReturnsFour()
+    {
+        var height = new int[] { 2, 0, 2 };
+
+        var result = MaxArea(height);
+
+        Assert.AreEqual(4, result);
+    }
+
+    [TestMethod]
+    public void HeightOneZeroTwo_ReturnsTwo()
+    {
+        var height = new int[] { 1, 0, 2 };
+
+        var result = MaxArea(height);
+
+        Assert.AreEqual(2, result);
+    }
+
+    [TestMethod]
+    public void HeightZero_ReturnsZero()
+    {
+        var height = new int[] { 0, 0 };
+
+        var result = MaxArea(height);
+
+        Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    public void BigHeightAtEnd_ReturnsFifty()
+    {
+        var height = new int[] { 1, 0, 1, 0, 1, 50, 50 };
+
+        var result = MaxArea(height);
+
+        Assert.AreEqual(50, result);
+    }
+
+    private int MaxArea(int[] height)
+    {
+        var @object = new LC011_ContainerWithMostWater();
+        var result = @object.MaxArea(height);
+        return result;
+    }
+}
